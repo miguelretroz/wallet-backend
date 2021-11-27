9ab2852bcee4af65d5bb452cc51adb50a066465c
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-11-27 17:19:54 -0300

    Install `ESLint`

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
