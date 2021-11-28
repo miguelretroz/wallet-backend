c9689736bd8bb06ca6862dde54fc554d4cc60bdc
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-11-28 20:47:08 -0300

    Add schema in `user` create service

--- services/user/create.js
+++ services/user/create.js
@@ -1,7 +1,10 @@
 const { UserModels } = require('../../models');
+const { UserSchemas } = require('../../schemas');
 
 module.exports = async (userData) => {
   const { repeatPassword, ...userDataWithoutRepeat } = userData;
+
+  UserSchemas.registerInput(userData);
 
   const newUser = UserModels.create(userDataWithoutRepeat);
 
