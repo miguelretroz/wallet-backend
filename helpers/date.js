61f2a8484c17d1e27967402c75a96f9e8a7205dc
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-12-25 08:56:46 -0300

    Fix date format regex

--- helpers/date.js
+++ helpers/date.js
@@ -1,4 +1,4 @@
-const dateFormatRegex = /(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/((19|20)\d{2})/;
+const dateFormatRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/((19|20)\d{2})$/;
 
 const thisDateExits = (date) => {
   try {
