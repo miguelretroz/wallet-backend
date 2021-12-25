f0b1cb334a3806364da6dc745daaccad6f468927
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-12-25 07:49:21 -0300

    Changes the date regex to the `mm-dd-yyyy` format

--- helpers/regex.js
+++ helpers/regex.js
@@ -3,10 +3,10 @@ const email = /^[\w.!#$%&'*+/=?^-`{|}~-]+@[a-zA-Z\d](?:[a-zA-Z\d-]{0,61}[a-zA-Z\
 
 const password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*()-+_={}[\]~^?]).{8,}$/;
 
-const date = /^(([1-9]|0[1-9])|1\d|2\d|3[01])\/((\d|0\d)|1[0-2])\/((19|20)\d{2})$/;
+const dateFormat = /^((0\d)|1[0-2])\/((0[1-9])|1\d|2\d|3[01])\/((19|20)\d{2})$/;
 
 module.exports = {
   email,
   password,
-  date,
+  dateFormat,
 };
