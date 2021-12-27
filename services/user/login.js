23f271be03e2fad3ec3423d2a530c66888864989
Miguel Retroz <contato.miguelretroz@gmail.com>
2021-12-27 08:24:34 -0300

    [User login route] [JWT authentication] (#18)
    * Generate `JWT token` with user id
    
    * Returns token in response to request

--- services/user/login.js
+++ services/user/login.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const { UserSchemas } = require('../../schemas');
 const { UserModels } = require('../../models');
@@ -8,16 +9,32 @@ const {
   errors,
 } = require('../../errors');
 
-module.exports = async ({ email, password }) => {
-  UserSchemas.emailValidator(email);
+const { JWT_SECRET_KEY } = process.env;
 
-  UserSchemas.passwordValidator(password);
+module.exports = async (userData) => {
+  UserSchemas.emailValidator(userData.email);
 
-  const user = await UserModels.getByEmail(email);
+  UserSchemas.passwordValidator(userData.password);
+
+  const user = await UserModels.getByEmail(userData.email);
 
   if (!user) return customError(errors.userNotFound());
 
-  if (!(await bcrypt.compare(password, user.password))) {
+  if (!(await bcrypt.compare(userData.password, user.password))) {
     return customError(errors.incorrectPassword());
   }
+
+  const { _id } = user;
+
+  const data = {
+    sub: _id,
+    exp: Math.floor(Date.now() / 1000) + (60 * 60 * 48), // Transforma Date.now() que vem em milisegundos em segundos e adiciona a quantidade de dois dias em segundos ao total.
+  };
+
+  const token = jwt.sign(
+    data,
+    JWT_SECRET_KEY,
+  );
+
+  return token;
 };
