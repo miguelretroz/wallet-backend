e7939a93e8ae2522b1f94c2e1432267a73f96e4b
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-12-28 19:15:11 -0300

    Implement model to store expense

--- models/expense/create.js
+++ models/expense/create.js
@@ -1,5 +1,9 @@
 const connection = require('../connection');
 
-module.exports = async () => {
+module.exports = async (expenseData) => {
   const db = await connection();
+
+  const { insertedId } = await db.collection('user').insertOne(expenseData);
+
+  return { _id: insertedId, ...expenseData };
 };
