24b3bcd74c90eba6525ceeada388251b4f07f804
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-12-28 18:14:04 -0300

    Create function to check string fields

--- schemas/expense/registerInputs.js
+++ schemas/expense/registerInputs.js
@@ -1,1 +1,18 @@
-module.exports = () => {};
+const {
+  isString,
+} = require('../../helpers');
+
+const {
+  customError,
+  errors,
+} = require('../../errors');
+
+const stringFieldsValidator = (field, fieldName, minLength) => {
+  if (!field) return customError(errors.isRequired(fieldName));
+  if (!isString(field)) return customError(errors.incorrectType(fieldName, 'string'));
+  if (field.length < minLength) return customError(errors.shortLength(fieldName, minLength));
+};
+
+module.exports = ({
+}) => {
+};
